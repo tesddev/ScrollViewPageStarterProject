80c8091c72e4666ff1adeaadeea3940fddcf495a
tesddev <tesleem.amuda@gmail.com>
2022-10-27 14:59:06 +0100

    Initial Commit

---
+++ ScrollViewPageStarterProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollViewPageStarterProject
+//
+//  Created by GIGL iOS on 27/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
