fb2e5ce6ddc384d42d92eacbf2d4f1b38872545a
tesddev <tesleem.amuda@gmail.com>
2022-10-27 15:24:15 +0100

    add starter project and some views

--- ScrollViewPageStarterProject/ViewController.swift
+++ ScrollViewPageStarterProject/ViewController.swift
@@ -2,18 +2,99 @@
 //  ViewController.swift
 //  ScrollViewPageStarterProject
 //
-//  Created by GIGL iOS on 27/10/2022.
+//  Created by Tes on 27/10/2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "chevron.backward"), for: .normal)
+        button.tintColor = .black
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        button.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Personal Shopping"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    lazy var proceedButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Proceed to Checkout", for: .normal)
+        button.clipsToBounds = true
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        button.layer.cornerRadius = 4
+        button.layer.borderWidth = 1
+        button.backgroundColor = .black
+        button.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+        constraintViews()
     }
-
-
+    
+    func constraintViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(backButton)
+        contentView.addSubview(headerLabel)
+        contentView.addSubview(proceedButton)
+        
+        scrollView.backgroundColor = UIColor.white
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = false
+        
+        NSLayoutConstraint.activate([
+            
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 25),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            
+            backButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            backButton.heightAnchor.constraint(equalToConstant: 28),
+            backButton.widthAnchor.constraint(equalToConstant: 28),
+            
+            headerLabel.centerYAnchor.constraint(equalTo: backButton.centerYAnchor),
+            headerLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            
+            proceedButton.topAnchor.constraint(equalTo: headerLabel.bottomAnchor, constant: 1300),
+            proceedButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            proceedButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.9),
+            proceedButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            proceedButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -40),
+        ])
+    }
+    @objc func didTapBackButton() {
+        navigationController?.popViewController(animated: true)
+    }
+    @objc func didTapLoginButton() {
+        print("login successful")
+    }
 }
-
